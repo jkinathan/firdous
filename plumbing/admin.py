903fe340521c49bb742f7e447786911023aaddec
jkinathan <jkinobe@gmail.com>
2022-04-25 17:20:52 +0300

    feat: Registers tables logic for Admin panel

--- plumbing/admin.py
+++ plumbing/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['inventoryPart','picture_tag','unitMeasure', 'costPrice','quantity','vendorSupplied']
+
+admin.site.register(Vendor),
+admin.site.register(Stock,StockAdmin),
+admin.site.register(Customer),
+admin.site.register(ExchangeRate),
